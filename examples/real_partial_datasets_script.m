1e7ccf7bde9525d0e1a1a1fda7a4c9510217ec4f
nicolas-e-b <140686385+nicolas-e-b@users.noreply.github.com>
2026-03-08 12:04:55 +0100

    Add files via upload

---
+++ examples/real_partial_datasets_script.m
@@ -0,0 +1,31 @@
+clear all
+load("Mirflickr.mat")
+
+
+dataset =data;
+partial_target = full(partial_target);
+target = full(target);
+
+
+
+prop_train = 0.8;  %train-test split
+T = 5;           % max number of iterations
+noise = 0.0;       % single noise level, change as needed
+c_1=0.5;           % the constant of the treshold
+rng(41);
+
+% --- Generate partial labels ---
+ partial_target = real_partial_datasets_with_noise(data, target, partial_target, noise);
+
+% --- Split ---
+[train, test] = dataset_split_part_labels(data, partial_target, target, prop_train);
+
+% --- Preprocess ---
+[train, test] = nn_preprocess_real_partial_datasets(train, test);
+
+% --- Predict ---
+pred = PL_Aknn_classifier(train, test, T, c_1);
+
+% --- Accuracy ---
+acc = mean(arrayfun(@(i) test.target(pred(i), i) > 0, 1:size(test.data, 1)));
+fprintf("Noise %.1f: error = %.4f\n", noise, 1 - acc);
---
+++ examples/vision_script.m
@@ -0,0 +1,26 @@
+clear all
+load("cifar_googlenet.mat")
+dataset = cifar_raw;
+
+prop_train = 0.8;  %train-test split
+T = 400;           % max number of iterations
+noise = 0.2;       % single noise level, change as needed
+c_1=0.5;           % the constant of the treshold
+num_clusters=5;    % numbers of clusters for the candidate generation
+rng(42);
+
+% --- Generate partial labels ---
+[data, target, partial_target] = candidate_generator_vision(dataset, noise, num_clusters);
+
+% --- Split ---
+[train, test] = dataset_split_part_labels(data, partial_target, target, prop_train);
+
+% --- Preprocess ---
+[train, test] = nn_preprocess_vision(train, test);
+
+% --- Predict ---
+pred = PL_Aknn_classifier(train, test, T, c_1);
+
+% --- Accuracy ---
+acc = mean(arrayfun(@(i) test.target(pred(i), i) > 0, 1:size(test.data, 1)));
+fprintf("Noise %.1f: error = %.4f\n", noise, 1 - acc);
